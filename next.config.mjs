7708adff3508cf7f5325ee0ee2fd4edaf10a622e
Illia Lynnyk <illyalinnyk@gmail.com>
2026-03-27 14:17:33 -0400

    Updated next.js basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
+console.log('isProd', isProd);
+
 const nextConfig = {
   output: 'export',
-  basePath: '/garys-garage',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/garys-garage' : '',
+  assetPrefix: isProd ? '/garys-garage/' : '',
+  trailingSlash: true,
 }
 
 export default nextConfig
